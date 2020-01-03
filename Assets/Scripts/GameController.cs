62a110a31e1c7624056a7f731e04d01f2f9b792e
AlbertoVf <AlbertoVf97@protonmail.com>
2020-01-03 13:35:22 +0100

    Inicio
    Creacion de pantalla de inicio e informacion

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,15 +13,50 @@ public class GameController : MonoBehaviour
     /// </summary>
     public RawImage background;
 
+    public GameState gameState = GameState.Idle;
+
     /// <summary>
     /// The platform.
     /// Escenario sobre el que se moveran los personajes.
     /// </summary>
     public RawImage platform;
 
+    public GameObject uiIdle;
+
     #endregion Public Fields
 
+    #region Public Enums
+
+    public enum GameState { Idle, Playing };
+
+    #endregion Public Enums
+
     #region Private Methods
+
+    /// <summary>
+    /// Paralaxes this instance.
+    /// Ejecucion de un efecto paralax al fondo y la plataforma.
+    /// </summary>
+    private void Paralax()
+    {
+        float finalSpeed = Speeds.PARALAX * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 2f, 0f, 1f, 1f);
+    }
+
+    /// <summary>
+    /// Pausars the iniciar.
+    /// Comprueba si el juego se inicia.
+    /// </summary>
+    /// <returns>true si se inicia el juego</returns>
+    private bool PausarIniciar()
+    {
+        if (Input.GetKeyDown(KeyControllers.START) || Input.GetMouseButtonDown(MouseControllers.START))
+        {
+            return true;
+        }
+        return false;
+    }
 
     // Start is called before the first frame update
     private void Start()
@@ -29,13 +65,20 @@ public class GameController : MonoBehaviour
 
     /// <summary>
     /// Updates this instance.
-    /// Ejecucion de un efecto paralax al fondo y la plataforma
+    /// Iniciar el juego.
     /// </summary>
     private void Update()
     {
-        float finalSpeed = Speeds.PARALAX * Time.deltaTime;
-        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
-        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 2f, 0f, 1f, 1f);
+        if (gameState == GameState.Idle && PausarIniciar())
+        {
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+        }
+
+        if (gameState == GameState.Playing)
+        {
+            Paralax();
+        }
     }
 
     #endregion Private Methods
--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -1,5 +1,42 @@
-﻿namespace Assets.Scripts
+﻿using UnityEngine;
+
+namespace Assets.Scripts
 {
+    /// <summary>
+    /// Clase para gestionar las teclas.
+    /// </summary>
+    public class KeyControllers
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// The jump
+        /// Tecla para realizar un salto
+        /// </summary>
+        public const KeyCode JUMP = KeyCode.Space;
+
+        /// <summary>
+        /// The start
+        /// Tecla para iniciar el juego.
+        /// </summary>
+        public const KeyCode START = KeyCode.Return;
+
+        #endregion Public Fields
+    }
+
+    public class MouseControllers
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// The start.
+        /// Boton primario del raton.
+        /// </summary>
+        public const int START = 0;
+
+        #endregion Public Fields
+    }
+
     /// <summary>
     /// Clase para gestionar las velocidades del juego.
     /// </summary>
