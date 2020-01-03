2466982d9222200dc8fb5faa8e1b4701f2a5c539
AlbertoVf <AlbertoVf97@protonmail.com>
2020-01-03 12:49:33 +0100

    Paralax
    Creacion de un efecto paralax

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    #region Public Fields
+
+    /// <summary>
+    /// The background.
+    /// Fondo del escenario
+    /// </summary>
+    public RawImage background;
+
+    /// <summary>
+    /// The platform.
+    /// Escenario sobre el que se moveran los personajes.
+    /// </summary>
+    public RawImage platform;
+
+    #endregion Public Fields
+
+    #region Private Methods
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    /// <summary>
+    /// Updates this instance.
+    /// Ejecucion de un efecto paralax al fondo y la plataforma
+    /// </summary>
+    private void Update()
+    {
+        float finalSpeed = Speeds.PARALAX * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 2f, 0f, 1f, 1f);
+    }
+
+    #endregion Private Methods
+}
---
+++ Assets/Scripts/General.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts
+{
+    /// <summary>
+    /// Clase para gestionar las velocidades del juego.
+    /// </summary>
+    public class Speeds
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// The paralax.
+        /// Velocidad delfondo
+        /// </summary>
+        public const float PARALAX = 0.05f;
+
+        #endregion Public Fields
+    }
+}
